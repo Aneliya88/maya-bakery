fd86c9c4c13f2a22b189a1b38baad4c09738959b
aneliya88 <aneliya.b@yahoo.com>
2024-09-15 13:20:22 -0500

    message for me

--- src/Header.js
+++ src/Header.js
@@ -10,12 +10,12 @@ const Header = () => {
     <div className="header">
       <div className="headerText">
         <img className='icon' src={IconLocation} alt="Location Icon"/>
-        <p>{language === 'en' ? 'Chicago, Illinois, US' : 'Чикаго, Иллинойс, США'}</p>
+        <p className='location'>{language === 'en' ? 'Chicago, Illinois, US' : 'Чикаго, Иллинойс, США'}</p>
       </div>
       <img className='logo' src={logo} alt="Logo"/>
       <div className="headerText">
         <img className='icon' src={IconPhone} alt="Phone Icon"/>
-        <a className='contact' href='tel:+13125508916'>
+        <a className='contact location' href='tel:+13125508916'>
           {language === 'en' ? '+1 (312) 550 89 16' : '+7 (312) 550 89 16'}
         </a>
       </div>
